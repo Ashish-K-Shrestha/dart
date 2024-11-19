4793acf233170b3c12bf1162701aff6b4af710c5
Ashish-K-Shrestha <126065324+Ashish-K-Shrestha@users.noreply.github.com>
2024-11-19 07:34:03 +0545

    Random Number Generator
    code of Random Number Generator

---
+++ program5.dart
@@ -0,0 +1,7 @@
+import 'dart:math';
+
+void main() {
+  Random random = Random();
+  int randomNumber = random.nextInt(100); // Random number between 0 and 99
+  print("Generated Random Number: $randomNumber");
+}
